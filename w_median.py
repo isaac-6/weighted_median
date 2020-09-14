8a28726fad9601d9154001648e00b1ecc270a5cc
isaac-6 <41800778+isaac-6@users.noreply.github.com>
2020-09-14 10:19:28 +0200

    Create w_median.py

---
+++ w_median.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def wm(preds, weights):
+'''
+Returns the weighted median of a single one-dimentional array given the weights.
+It interpolates linearly between the two points closest to the actual median.
+'''
+    idx = np.argsort(preds)
+    vs = preds[idx]
+    sw = weights[idx]
+    swc = sw.cumsum()
+    th = swc[-1]/2
+    p2 = np.where(swc > th)[0][0]
+    p1 = p2 - 1
+    wMedian = (vs[p1]*(swc[p2] - th) + vs[p2]*(th -swc[p1]))/(swc[p2]-swc[p1])
+    return wMedian
+ 
+
+def weighted_median(preds, weights):
+'''
+Returns the weighted median of a multi-dimentional array given the weights of the columns.
+It can be useful to compute the weighted mean of different predictors.
+'''
+    wmpred = [wm(preds[i,:],weights) for i in range(len(preds))]
+    return wmpred
